2a399ff09be1f65e3d812be18d7fe2b2ed0fd33a
Barreras Lautaro Exequiel <l.barreras@etec.local>
2023-08-16 12:40:58 -0300

    Arreglo parte del Nav

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,23 @@
 import './header.css';
 import LogoMenu from '../../multimedia/LogoMenu.png';
-import LogoOzonoSensitive from '../../multimedia/LogoOzonoSensitive.png';
+import LogoSinNombreSinFondo from '../../multimedia/LogoSinNombreSinFondo.png';
 
 
 export default function Header() {
 
     return (
         <header>
-            <img src={LogoOzonoSensitive} alt="Logo" className="LogoNav" />
+            
             <input type="checkbox" id="btn-menu"/>
             <label id="ham-menu" for="btn-menu"><img src={LogoMenu} alt=""/></label>
             <nav className="menu" id="conteiner">
+                <img src={LogoSinNombreSinFondo} alt="Logo" className="LogoNav" />
                 <ul className="BotonesNav">
                     <li><a href="">Acerca de nosotros</a></li>
                     <li><a href="">Productos</a></li>
                     <li><a href="">Contacto</a></li>
                 </ul>
-                <ul className="carrito-usuario">
+                <ul className="CarritoUsuario">
                     <li><a href="./carrito.html"><span className="material-symbols-outlined">shopping_cart</span></a></li>
                     <li><a href="./usuario.html"><span className="material-symbols-outlined">account_circle</span></a></li>
                 </ul>
